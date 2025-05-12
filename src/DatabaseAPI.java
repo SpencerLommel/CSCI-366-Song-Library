8dd50f02aca2f799cfcba3cb1c3e634e5ab7efd3
SpencerLommel <spencerlommel@gmail.com>
2025-05-11 22:48:18 -0500

    databaseAPI and input

--- src/DatabaseAPI.java
+++ src/DatabaseAPI.java
@@ -9,11 +9,37 @@ public class DatabaseAPI {
         this.connection = dbConn.getConnection();
     }
 
-
-
     // ====================================
     //  Song Methods
     // ====================================
+
+    public void displaySongsWithAlbumAndArtist(String songSearch) {
+        String query = """
+    SELECT s.song_id, s.title, s.release_year, al.title AS album_title, ar.name AS artist_name, s.duration
+    FROM song s
+    JOIN album al ON s.album_id = al.album_id
+    JOIN artist ar ON s.artist_id = ar.artist_id
+    WHERE s.title ILIKE ?
+    """;
+        runQueryToTable(query, "%" + songSearch + "%");
+    }
+
+
+    public int getArtistIdFromSong(int songId) {
+        String query = "SELECT artist_id FROM song WHERE song_id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, songId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("artist_id");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; // Not found
+    }
+
 
     // searchSong(String song_name)
     public void searchSong(String searchName) {
@@ -25,17 +51,53 @@ public class DatabaseAPI {
     //  Playlist Methods
     // ====================================
 
+    public void deletePlaylistById(int playlistId) {
+        String query = "DELETE FROM playlist WHERE playlist_id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, playlistId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void displaySongsInPlaylistWithDetails(int playlistID) {
+        String query = """
+    SELECT s.song_id, s.title, s.release_year, al.title AS album_title, ar.name AS artist_name, s.duration
+    FROM playlist_song ps
+    JOIN song s ON ps.song_id = s.song_id
+    JOIN album al ON s.album_id = al.album_id
+    JOIN artist ar ON s.artist_id = ar.artist_id
+    WHERE ps.playlist_id = ?
+    """;
+        runQueryToTable(query, playlistID);
+    }
+
+
     // searchPlaylist(String playlist_name)
     public void searchPlaylist(String playlistName) {
         searchTable("playlist", "name", playlistName);
     }
 
-    // create playlist (playlist_name)
+    // create playlist (playlist_name) [using Statement]
+//    public void createPlaylist(String playlistName) {
+//        try(Statement s = connection.createStatement()) {
+//            String query = "INSERT INTO playlist (name) VALUES ('" + playlistName + "')";
+//            int count = s.executeUpdate(query);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    // createPlaylist playlist (playlistName) [using PreparedStatement]
     public void createPlaylist(String playlistName) {
-        try(Statement s = connection.createStatement()) {
-            String query = "INSERT INTO playlist (name) VALUES ('" + playlistName + "')";
-            int count = s.executeUpdate(query);
+        String query = "INSERT INTO playlist (name) VALUES (?)";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, playlistName);
+            int count = ps.executeUpdate();
         } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
@@ -44,11 +106,101 @@ public class DatabaseAPI {
         deleteByValue("playlist", "name", playlistName );
     }
 
-    // add song to playlist (playlist_id, song_id)
+    // add song to playlist (playlist_id, song_id) [using Statement]
+//    public void addSongToPlaylist(int p_id, int s_id) {
+//        try(Statement s = connection.createStatement()) {
+//            String query = "INSERT INTO playlist_song (playlist_id, song_id) VALUES ('" + p_id + ", " + s_id +  "')";
+//            int count = s.executeUpdate(query);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    // remove song from playlist (playlist_id, song_id)
+    // add song to playlist (playlist_id, song_id) [using PreparedStatement]
+    public void addSongToPlaylist(int p_id, int s_id) {
+        String query = "INSERT INTO playlist_song (playlist_id, song_id) VALUES (?, ?)";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, p_id);
+            ps.setInt(2, s_id);
+            int count = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // remove song from playlist (playlist_id, song_id) [using Statement]
+//    public void removeSongFromPlaylist(int p_id, int s_id) {
+//        try(Statement s = connection.createStatement()) {
+//            String query = "DELETE FROM playlist_song WHERE playlist_id = " + p_id + " AND song_id = " + s_id;
+//            int count = s.executeUpdate(query);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    // remove song from playlist (playlist_id, song_id) [using PreparedStatement]
+    public void removeSongFromPlaylist(int p_id, int s_id) {
+        String query = "DELETE FROM playlist_song WHERE playlist_id = ? AND song_id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, p_id);
+            ps.setInt(2, s_id);
+            int count = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //get Playlist Length
+    public void getPlaylistDuration(int playlistID) {
+        String query = """
+        SELECT SUM(s.duration) AS total_duration
+        FROM playlist_song ps
+        JOIN song s ON ps.song_id = s.song_id
+        WHERE ps.playlist_id = ?
+        """;
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, playlistID);
 
 
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    int totalDuration = rs.getInt("total_duration");
+                    System.out.println("Total duration for playlist ID " + playlistID + " is: " + totalDuration + " seconds.");
+                } else {
+                    System.out.println("No songs found for playlist ID " + playlistID + ".");
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //update playlist name
+    public void updatePlaylistName(int playlistID, String newName) {
+        String query = "UPDATE playlist SET name = ? WHERE playlist_id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, newName);
+            ps.setInt(2, playlistID);
+            int count = ps.executeUpdate();
+            if (count == 0) {
+                System.out.println("No playlist found with ID: " + playlistID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //get all songs on a Playlist
+    public void getSongsInPlaylist(int playlistID) {
+        String query = """
+        SELECT s.*
+        FROM playlist_song ps
+        JOIN song s ON ps.song_id = s.song_id
+        WHERE ps.playlist_id = ?
+        """;
+        runQueryToTable(query, playlistID);
+    }
     // ====================================
     //  Artist Methods
     // ====================================
@@ -75,17 +227,75 @@ public class DatabaseAPI {
 
 
     // get Artist Songs
+    public void getArtistSongs(int artistID) {
+        String query = """
+        SELECT so.*, ar.*
+        FROM "song" so
+        JOIN "artist" ar ON ar.artist_id = so.artist_id
+        WHERE ar.artist_id = %d
+        """.formatted(artistID);
+
+        runQueryToTable(query);
+    }
 
     // ====================================
     //  Album Methods
     // ====================================
+
+    public void displaySongsInAlbumWithDetails(int albumID) {
+        String query = """
+    SELECT s.song_id, s.title, s.release_year, al.title AS album_title, ar.name AS artist_name, s.duration
+    FROM song s
+    JOIN album al ON s.album_id = al.album_id
+    JOIN artist ar ON s.artist_id = ar.artist_id
+    WHERE s.album_id = ?
+    """;
+        runQueryToTable(query, albumID);
+    }
+
+
+    public void displayAlbumsWithArtist(String albumSearch) {
+        String query = """
+    SELECT al.album_id, al.title, al.release_year, ar.name AS artist_name
+    FROM album al
+    JOIN artist ar ON al.artist_id = ar.artist_id
+    WHERE al.title ILIKE ?
+    """;
+        runQueryToTable(query, "%" + albumSearch + "%");
+    }
+
 
     // search Album
     public void searchAlbum(String albumName) {
         searchTable("album", "title", albumName);
     }
 
+    // get Album Duration
+    public void getAlbumDuration(int albumID) {
+        String query = """
+    SELECT SUM(s.duration) AS total_duration
+    FROM song s
+    WHERE s.album_id = ?
+    """;
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, albumID);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    int totalDuration = rs.getInt("total_duration");
+                    System.out.println("Total duration for album ID " + albumID + " is: " + totalDuration + " seconds.");
+                } else {
+                    System.out.println("No songs found for album ID " + albumID + ".");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    public void getSongsInAlbum(int albumID) {
+        runQueryToTable("SELECT * FROM song WHERE album_id = ?", albumID);
+    }
 
 
     // ====================================
@@ -116,42 +326,53 @@ public class DatabaseAPI {
     }
 
     // This runs a query and outputs a nice table
-    private void runQueryToTable(String query) {
-        try (Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
-
-            ResultSetMetaData meta = rs.getMetaData();
-            int columnCount = meta.getColumnCount();
-
-            // Collect headers
-            String[] headers = new String[columnCount];
-            for (int i = 1; i <= columnCount; i++) {
-                headers[i - 1] = meta.getColumnName(i);
+    // Here's some examples of the usage
+    // runQueryToTable("SELECT * FROM song");
+    // or to use a parameter you can do
+    // runQueryToTable("SELECT * FROM song WHERE artist_id = ?", 1);
+    private void runQueryToTable(String query, Object... params) {
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            // Bind parameters if provided
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
             }
 
-            // Collect rows
-            ArrayList<String[]> rowsList = new ArrayList<>();
-            while (rs.next()) {
-                String[] row = new String[columnCount];
+            try (ResultSet rs = pstmt.executeQuery()) {
+                ResultSetMetaData meta = rs.getMetaData();
+                int columnCount = meta.getColumnCount();
+
+                // Collect headers
+                String[] headers = new String[columnCount];
                 for (int i = 1; i <= columnCount; i++) {
-                    row[i - 1] = rs.getString(i);
+                    headers[i - 1] = meta.getColumnName(i);
                 }
-                rowsList.add(row);
-            }
 
-            String[][] rows = rowsList.toArray(new String[0][]);
-            printTable(headers, rows);
+                // Collect rows
+                ArrayList<String[]> rowsList = new ArrayList<>();
+                while (rs.next()) {
+                    String[] row = new String[columnCount];
+                    for (int i = 1; i <= columnCount; i++) {
+                        row[i - 1] = rs.getString(i);
+                    }
+                    rowsList.add(row);
+                }
+
+                String[][] rows = rowsList.toArray(new String[0][]);
+                printTable(headers, rows);
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+
     // generic searchTable where you can pass a table name a search param
     public void searchTable(String tableName, String columnName, String searchParam) {
         String query = "SELECT * FROM \"" + tableName + "\" WHERE \"" + columnName + "\" ILIKE '%" + searchParam + "%'";
         runQueryToTable(query);
     }
+
 
     public void deleteByValue(String tableName, String columnName, String valueToDelete) {
         String query = "DELETE FROM \"" + tableName + "\" WHERE \"" + columnName + "\" = ?";
--- src/Input.java
+++ src/Input.java
@@ -4,80 +4,198 @@ import java.util.Scanner;
 
 public class Input {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DatabaseAPI dbAPI = new DatabaseAPI();
+
     public static void userInput() {
-        DatabaseAPI dbAPI = new DatabaseAPI();
-
-        // we start with outputting the main dialog window and then most stuff is based off this value
-        int selection = displayMainMenu();
-        System.out.println(selection);
-
-        switch (selection) {
-            case 1:
-                System.out.println("You entered 1");
-                String artistName = stringInput("Enter artist name: ");
-                dbAPI.searchArtist(artistName);
-
-            case 2:
-                String playlistName = stringInput("Enter playlist name: ");
-                dbAPI.deletePlaylist(playlistName);
-        }
-
-
-    }
-
-
-    public static int displayArtistsMenu() {
-        String artist = stringInput("Enter an artist: ");
-
-        return 0;
-
-    }
-
-    public static int displayMainMenu() {
-        return selectorList("Enter a number:", "Artists", "Playlists", "Quit");
-
-    }
-
-
-    // this function allows you to pass in a title and options and returns an int based on what you selected
-    // NOTE: we return a base index 1 selection. So if a user selects the first option we return 1 instead of 0
-    private static int selectorList(String title, String... options) {
-        Scanner scanner = new Scanner(System.in);
-
-        // print title
-        System.out.println(title);
-        // print all the options from the method
-        for (int i = 0; i < options.length; i++) {
-            System.out.printf("%d: %s%n", i+1, options[i]);
-        }
-
-        int valueSelection;
         while (true) {
-            if (scanner.hasNextInt()) {
-                valueSelection = scanner.nextInt();
-                if (valueSelection >= 1 && valueSelection < options.length) {
-                    return valueSelection;
-                } else {
-                    System.out.printf("Please input an integer between 1 and %d:\n", options.length);
-                    for (int i = 0; i < options.length; i++) {
-                        System.out.printf("%d: %s%n", i+1, options[i]);
-                    }
-                }
-
-            } else {
-                scanner.next(); // gets rid of non int inputs
-                System.out.printf("Please input an integer between 1 and %d:\n", options.length);
-                for (int i = 0; i < options.length; i++) {
-                    System.out.printf("%d: %s%n", i+1, options[i]);
-                }
+            int selection = displayMainMenu();
+            switch (selection) {
+                case 1 -> handleArtistsMenu();
+                case 2 -> handlePlaylistsMenu();
+                case 3 -> handleSongsMenu();
+                case 4 -> handleAlbumsMenu();
+                case 5 -> { System.out.println("Goodbye!"); return; }
             }
         }
     }
 
-    private static String stringInput(String title) {
+    private static void handleArtistsMenu() {
+        while (true) {
+            String artistName = stringInput("Enter artist name (or press ENTER to list all, or type 'back' to go back): ");
+            if (artistName.equalsIgnoreCase("back")) return;
+
+            if (artistName.isBlank()) {
+                dbAPI.searchArtist("");
+            } else {
+                dbAPI.searchArtist(artistName);
+            }
+
+            int artistId = intInput("Enter Artist ID to view profile (or 0 to go back): ");
+            if (artistId == 0) continue;
+
+            dbAPI.getArtistAlbums(artistId);
+            dbAPI.getArtistSongs(artistId);
+        }
+    }
+
+    private static void handlePlaylistsMenu() {
+        while (true) {
+            int action = selectorList("What would you like to do?", "Create New Playlist", "Search Existing Playlist", "Back");
+            if (action == 3) return;
+
+            if (action == 1) {
+                String newName = stringInput("Enter new playlist name: ");
+                dbAPI.createPlaylist(newName);
+                System.out.println("Playlist created.");
+                continue;
+            }
+
+            // Search or List
+            String playlistName = stringInput("Enter playlist name (or press ENTER to list all, or type 'back' to go back): ");
+            if (playlistName.equalsIgnoreCase("back")) return;
+
+            if (playlistName.isBlank()) {
+                dbAPI.searchPlaylist(""); // Lists all playlists
+            } else {
+                dbAPI.searchPlaylist(playlistName); // Filtered search
+            }
+
+            int playlistId = intInput("Enter Playlist ID to manage (or 0 to go back): ");
+            if (playlistId == 0) continue;
+
+            managePlaylist(playlistId);
+        }
+    }
+
+
+    private static void handleSongsMenu() {
+        while (true) {
+            String songName = stringInput("Enter song title (or press ENTER to list all, or type 'back' to go back): ");
+            if (songName.equalsIgnoreCase("back")) return;
+
+            if (songName.isBlank()) {
+                dbAPI.displaySongsWithAlbumAndArtist(""); // List all songs with album/artist
+            } else {
+                dbAPI.displaySongsWithAlbumAndArtist(songName); // Filtered search
+            }
+
+            int songId = intInput("Enter Song ID to manage (or 0 to go back): ");
+            if (songId == 0) continue;
+
+            int action = selectorList("What would you like to do with this song?", "Add to Playlist", "View Artist", "Back");
+            switch (action) {
+                case 1 -> {
+                    dbAPI.searchPlaylist("");  // List all playlists
+                    int playlistId = intInput("Enter Playlist ID to add this song to (or 0 to cancel): ");
+                    if (playlistId == 0) continue;
+                    dbAPI.addSongToPlaylist(playlistId, songId);
+                    System.out.println("Song added to playlist.");
+                }
+                case 2 -> {
+                    int artistId = dbAPI.getArtistIdFromSong(songId);
+                    if (artistId == -1) {
+                        System.out.println("Artist not found for this song.");
+                    } else {
+                        System.out.println("Opening artist profile...");
+                        dbAPI.getArtistAlbums(artistId);
+                        dbAPI.getArtistSongs(artistId);
+                    }
+                }
+                case 3 -> {
+                    continue;
+                }
+            }
+
+        }
+    }
+
+
+    private static void handleAlbumsMenu() {
+        while (true) {
+            String albumName = stringInput("Enter album name (or press ENTER to list all, or type 'back' to go back): ");
+            if (albumName.equalsIgnoreCase("back")) return;
+
+            if (albumName.isBlank()) {
+                dbAPI.displayAlbumsWithArtist(""); // List all albums with artist name
+            } else {
+                dbAPI.displayAlbumsWithArtist(albumName); // Filtered search
+            }
+
+            int albumId = intInput("Enter Album ID to view songs (or 0 to go back): ");
+            if (albumId == 0) continue;
+
+            dbAPI.displaySongsInAlbumWithDetails(albumId);  // Show all songs with album/artist details
+            dbAPI.getAlbumDuration(albumId);                // Show album duration
+
+        }
+    }
+
+    private static void managePlaylist(int playlistId) {
+        while (true) {
+            int action = selectorList("Manage Playlist:", "View Songs", "Edit Name", "Delete Playlist", "Back");
+            switch (action) {
+                case 1 -> managePlaylistSongs(playlistId);
+
+                case 2 -> {
+                    String newName = stringInput("Enter new playlist name: ");
+                    dbAPI.updatePlaylistName(playlistId, newName);
+                    System.out.println("Playlist name updated.");
+                }
+                case 3 -> {
+                    dbAPI.deletePlaylistById(playlistId);
+                    System.out.println("Playlist deleted.");
+                    return;
+                }
+                case 4 -> { return; }
+            }
+        }
+    }
+
+
+    private static int displayMainMenu() {
+        return selectorList("Enter a number:", "Artists", "Playlists", "Songs", "Album", "Quit");
+    }
+
+    private static int selectorList(String title, String... options) {
         System.out.println(title);
-        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < options.length; i++) {
+            System.out.printf("%d: %s%n", i + 1, options[i]);
+        }
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid number:");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    private static String stringInput(String prompt) {
+        System.out.println(prompt);
+        scanner.nextLine(); // Consume leftover newline
         return scanner.nextLine();
+    }
+
+    private static int intInput(String prompt) {
+        System.out.println(prompt);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid integer:");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    private static void managePlaylistSongs(int playlistId) {
+        while (true) {
+            dbAPI.displaySongsInPlaylistWithDetails(playlistId);
+            dbAPI.getPlaylistDuration(playlistId);
+
+            int songId = intInput("Enter Song ID to remove (or 0 to go back): ");
+            if (songId == 0) return;
+
+            dbAPI.removeSongFromPlaylist(playlistId, songId);
+            System.out.println("Song removed from playlist.");
+        }
     }
 
 }
