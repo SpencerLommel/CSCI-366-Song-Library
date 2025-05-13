398e1c3febc137c3df67b91ce778dcabfd0f2f22
SpencerLommel <spencerlommel@gmail.com>
2025-05-13 12:00:58 -0500

    user input and database connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 
 public class DatabaseConnection {
+    // this connection string is for a dev database and will only be valid until the end of May and then it will be deleted
     private static final String URL = "jdbc:postgresql://postgres.spencerlommel.com:5432/csci-366-project";
     private static final String USER = "csci-366-project";
     private static final String PASSWORD = "csci-366-project";
